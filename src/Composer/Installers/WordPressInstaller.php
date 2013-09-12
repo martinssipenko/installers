1ffc282af6c77bb6b2f55d6c9113515f49834d04
Shawn Stratton <shawn_stratton@discovery.com>
2013-09-12 18:26:26 +0000

    Sub Directory, should probably prefix it as dni-fusion instead of wp

--- src/Composer/Installers/WordPressInstaller.php
+++ src/Composer/Installers/WordPressInstaller.php
@@ -4,9 +4,9 @@ namespace Composer\Installers;
 class WordPressInstaller extends BaseInstaller
 {
     protected $locations = array(
-        'plugin'    => 'wp-content/plugins/{$name}/',
-        'theme'     => 'wp-content/themes/{$name}/',
-        'muplugin'  => 'wp-content/mu-plugins/{$name}/',
-	'core'     => './',
+        'plugin'    => 'docs/wp-content/plugins/{$name}/',
+        'theme'     => 'docs/wp-content/themes/{$name}/',
+        'muplugin'  => 'docs/wp-content/mu-plugins/{$name}/',
+        'core'      => 'docs/',
     );
 }
